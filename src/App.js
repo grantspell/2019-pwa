21fd1a5425c5735d83da94bfa44551213da26a51
grantspell <spellgrant@gmail.com>
2019-04-06 17:18:21 -0400

    UPDATED | `src>App.js` with form and event handlers

--- src/App.js
+++ src/App.js
@@ -3,10 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+  
   render() {
     return (
       <div className="App">
-      
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Enter your name: </label>
+          <input
+            id="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.greeting}</p>
       </div>
     );
   }
